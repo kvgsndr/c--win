5bd2c31a0131e16d872a58dfa92900afc8990f25
Kővágó Sándor <56760241+kvgsndr@users.noreply.github.com>
2021-03-04 16:53:44 +0100

    20210303

---
+++ Tanulók kiválasztása/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Tanulók_kiválasztása
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btValaszt = new System.Windows.Forms.Button();
+            this.btHalaszt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btPrezental = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(68, 29);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Csopoort";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(25, 80);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(188, 329);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btValaszt
+            // 
+            this.btValaszt.Location = new System.Drawing.Point(244, 163);
+            this.btValaszt.Name = "btValaszt";
+            this.btValaszt.Size = new System.Drawing.Size(75, 23);
+            this.btValaszt.TabIndex = 3;
+            this.btValaszt.Text = "Választ";
+            this.btValaszt.UseVisualStyleBackColor = true;
+            this.btValaszt.Click += new System.EventHandler(this.btValaszt_Click);
+            // 
+            // btHalaszt
+            // 
+            this.btHalaszt.Location = new System.Drawing.Point(244, 213);
+            this.btHalaszt.Name = "btHalaszt";
+            this.btHalaszt.Size = new System.Drawing.Size(75, 23);
+            this.btHalaszt.TabIndex = 4;
+            this.btHalaszt.Text = "Halaszt";
+            this.btHalaszt.UseVisualStyleBackColor = true;
+            this.btHalaszt.Click += new System.EventHandler(this.btHalaszt_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(253, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(397, 41);
+            this.label2.TabIndex = 5;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btPrezental
+            // 
+            this.btPrezental.Location = new System.Drawing.Point(244, 265);
+            this.btPrezental.Name = "btPrezental";
+            this.btPrezental.Size = new System.Drawing.Size(75, 23);
+            this.btPrezental.TabIndex = 6;
+            this.btPrezental.Text = "Prezentál";
+            this.btPrezental.UseVisualStyleBackColor = true;
+            this.btPrezental.Click += new System.EventHandler(this.btPrezental_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(662, 435);
+            this.Controls.Add(this.btPrezental);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btHalaszt);
+            this.Controls.Add(this.btValaszt);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Ki fog felelni?";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btValaszt;
+        private System.Windows.Forms.Button btHalaszt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btPrezental;
+    }
+}
+
---
+++ Tanulók kiválasztása/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Tanulók_kiválasztása
+{
+    public partial class Form1 : Form
+    {
+        struct Tanulo
+        {
+            public int prezentalt;
+            public int halasztott;
+            public string nev;
+
+        }
+        List<Tanulo> lista = new List<Tanulo>();
+        int sorszam = 0;
+        string[] file_list;
+        int maxprez = 1;
+
+        private int maxPrezent()
+        { int max = lista[0].prezentalt;
+            foreach (var item in lista)
+                if (max < item.prezentalt) max = item.prezentalt;
+
+            return max;
+        }
+
+        private void listaMent()
+        {
+            try
+            {
+                StreamWriter fki = new StreamWriter(file_list[comboBox1.SelectedIndex]);
+                fki.WriteLine("prezentált;Név;halasztott");
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    fki.WriteLine($"{lista[i].prezentalt};{lista[i].nev};{lista[i].halasztott}");
+                }
+                fki.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Lista ment:{e.Message}");
+
+            }
+
+        }
+
+        string tanuloToString(Tanulo egyed)
+        {   var hala= egyed.halasztott > 0 ? "Halasztott x "+egyed.halasztott.ToString() : "";
+            return String.Format($"{egyed.prezentalt} - {egyed.nev} {hala}");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            file_list = Directory.GetFiles(@"..\..\Data");
+            for (int i = 0; i < file_list.Length; i++)
+            {
+                var utvonal_elemei = file_list[i].Split('\\');
+                string elem = utvonal_elemei[utvonal_elemei.Length - 1];
+                comboBox1.Items.Add(elem);
+            }
+            
+        }
+
+        private void btValaszt_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            do
+            {
+                sorszam = rnd.Next(lista.Count);
+            } 
+            while (lista[sorszam].prezentalt > maxprez-1 );
+
+            string hala = lista[sorszam].halasztott >0 ? "Halasztott" : "";
+            label2.Text = lista[sorszam].nev + " " + hala;
+            listBox1.SelectedIndex = sorszam;
+            listaMent();
+
+        }
+
+        private void btHalaszt_Click(object sender, EventArgs e)
+        {
+            Tanulo egyed = lista[sorszam];
+            egyed.halasztott+=1;
+            lista[sorszam] = egyed;
+            listBox1.Items[sorszam] = tanuloToString(egyed);
+            listaMent();
+        }
+
+        private void btPrezental_Click(object sender, EventArgs e)
+        {
+            Tanulo egyed = lista[sorszam];
+            egyed.prezentalt += 1;
+            egyed.halasztott = 0;
+            lista[sorszam] = egyed;
+            listBox1.Items[sorszam] = tanuloToString(egyed);
+            listaMent();
+        }
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            try
+            {
+                StreamReader fbe = new StreamReader(file_list[comboBox1.SelectedIndex]);
+
+                lista.Clear();
+                listBox1.Items.Clear();
+                fbe.ReadLine();
+                while (!fbe.EndOfStream)
+                {
+
+                    Tanulo egyed;
+                    string sor = fbe.ReadLine();
+                    var elemek = sor.Split(';');
+                    
+                    egyed.nev = elemek[1];
+                    egyed.halasztott = int.Parse(elemek[2]);
+                    egyed.prezentalt = int.Parse(elemek[0]);
+                    lista.Add(egyed);
+                    string s = elemek[0] + "--" + elemek[1];
+                    if (elemek[2] == "1") s = s + "  Halasztott";
+                    listBox1.Items.Add(s);
+                }
+                fbe.Close();
+                listaMent();
+                maxprez=maxPrezent();
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show($" Combo change: {ee.Message}");
+            }
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sorszam = listBox1.SelectedIndex;
+        }
+    }
+}
