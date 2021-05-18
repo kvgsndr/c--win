9e308dc372554eafcc80884fdd5f6e8d7de724cb
Kővágó Sándor <56760241+kvgsndr@users.noreply.github.com>
2021-05-18 13:35:16 +0200

    20210518

---
+++ adfgvxGUI/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace adfgvx
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lbKódtábla = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Az ellenőrzés eredménye:";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(281, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Kódtábla betöltése";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(45, 83);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(374, 154);
+            this.listBox1.TabIndex = 2;
+            // 
+            // lbKódtábla
+            // 
+            this.lbKódtábla.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbKódtábla.Location = new System.Drawing.Point(45, 257);
+            this.lbKódtábla.Name = "lbKódtábla";
+            this.lbKódtábla.Size = new System.Drawing.Size(374, 193);
+            this.lbKódtábla.TabIndex = 3;
+            this.lbKódtábla.Text = "A betöltött kódtábla:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 484);
+            this.Controls.Add(this.lbKódtábla);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ADFGVX gui";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label lbKódtábla;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ adfgvxGUI/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace adfgvx
+{
+    public partial class Form1 : Form
+    {
+        List<string> kod = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { kod.Clear();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader fbe = new StreamReader(openFileDialog1.FileName);
+                while (!fbe.EndOfStream)
+                    kod.Add(fbe.ReadLine());
+            }
+           
+            lbKódtábla.Text = "A betöltött kódtábla:\n\n";
+            foreach(var sor in kod)
+            {
+                foreach (var karakter in sor)
+                    lbKódtábla.Text += String.Format($"{karakter,5}" );
+                lbKódtábla.Text += "\n";
+            }
+
+        }
+
+        
+    }
+}
