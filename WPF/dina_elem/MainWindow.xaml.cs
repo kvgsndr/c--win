d2aff5d1f9f3306fbff1a1468ace57dac847e21e
kvgsndr <56760241+kvgsndr@users.noreply.github.com>
2021-09-29 23:31:05 +0200

    20210929

---
+++ WPF/dina_elem/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dina_elem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            din_elem_letrehoz();
+        }
+
+        public void din_elem_letrehoz()
+        {
+            Label lab1 = new Label();
+            lab1.Content = "Ez egy dinamikusan létrehozott elem!";
+            lab1.Name = "lblab1";
+
+            
+            gridpanel.Children.Add(lab1);
+        }
+    }
+}
---
+++ dinamikus gomb/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dinamikus_gomb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CreateDynamicButton();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void CreateDynamicButton()
+
+        {
+
+            // Create a Button object 
+
+            Button dynamicButton = new Button();
+
+
+
+            // Set Button properties
+
+            dynamicButton.Height = 40;
+
+            dynamicButton.Width = 300;
+
+            dynamicButton.BackColor = Color.Red;
+
+            dynamicButton.ForeColor = Color.Blue;
+
+            dynamicButton.Location = new Point(20, 150);
+
+            dynamicButton.Text = "I am Dynamic Button";
+
+            dynamicButton.Name = "DynamicButton";
+
+            dynamicButton.Font = new Font("Georgia", 16);
+
+
+
+            // Add a Button Click Event handler
+
+            dynamicButton.Click += new EventHandler(DynamicButton_Click);
+
+            // Add Button to the Form. Placement of the Button
+            // will be based on the Location and Size of button
+
+            Controls.Add(dynamicButton);
+
+        }
+
+        /// Button click event handler
+
+        private void DynamicButton_Click(object sender, EventArgs e)
+
+        {
+
+            MessageBox.Show("Dynamic button is clicked");
+
+        }
+    }
+}
