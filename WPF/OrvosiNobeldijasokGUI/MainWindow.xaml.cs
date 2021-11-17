5b7c603b48e4b454eb269974713364b76b88b847
kvgsndr <56760241+kvgsndr@users.noreply.github.com>
2021-11-17 09:09:52 +0100

    20211117

---
+++ WPF/OrvosiNobeldijasokGUI/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace OrvosiNobeldijasokGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(txbEv.Text=="" || txbNev.Text=="" || txbOrszag.Text=="" || txbSzH.Text=="")
+            {
+                MessageBox.Show("Töltsön ki minden mezőt!", "HIBA");
+            }
+            else if( int.Parse(txbEv.Text)<=1989)
+            {
+                MessageBox.Show("Hiba! Az évszám nem megfelelő!", "Mentés");
+            }
+            else
+            {
+                try
+                {
+                    StreamWriter fki = new StreamWriter("uj_dijazott.txt");
+                    fki.WriteLine("Év;Név;SzületésHalálozás;Országkód");
+                    fki.WriteLine($"{txbEv.Text};{txbNev.Text};{txbSzH.Text};{txbOrszag.Text}");
+                    txbEv.Clear();txbNev.Clear();txbOrszag.Clear();txbSzH.Clear();
+                }
+                catch(Exception )
+                {
+                    MessageBox.Show("Hiba az állomány írásánál!");
+                }
+                
+            }
+        }
+    }
+}
---
+++ WPF/karacsonyGUI/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace karacsonyGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+   
+
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 1; i <= 40; i++)
+            {
+                cbxNapok.Items.Add(i);
+            }
+
+        }
+
+        private void btHozzaad_Click(object sender, RoutedEventArgs e)
+        {  
+            
+            
+            int nap = int.Parse(cbxNapok.SelectedItem.ToString());
+            int elk = int.Parse(txbElkeszitett.Text);
+            int elad = int.Parse(txbEladott.Text);
+            if(elk<0 || elad<0)
+            {
+                lbHiba.Content = "Negatív számot nem adhat meg!";
+
+            }
+            else if(elk<elad)
+            {
+                lbHiba.Content = "Túl sok eladott angyalka!";
+            }
+            else
+            {
+                string str = string.Format($"{nap}.nap\t+{elk}\t -{elad}\t=\t{elk - elad} ");
+                lbxLista.Items.Add(str);
+                txbEladott.Text = "0";
+                txbElkeszitett.Text = "0";
+                cbxNapok.SelectedIndex = -1;
+                lbHiba.Content = "";
+            }
+            
+        }
+    }
+}
